5c9ea26c96cbe221291e4f184b5418f0e9df381e
Ismael Laet <ismaelabashi@hotmail.com>
2022-05-24 19:23:14 -0300

    Feat: Add action function

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -20,14 +20,26 @@ const create = async (req, res) => {
     res.redirect(`/room/${roomId}`);
 }
 
-const action = (req, res) => {
+const action = async (req, res) => {
+    const db = await Database();
     const { roomId, questionId, action } = req.params;
     const { password } = req.body;
+    const currentRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`);
+    const validPassword = currentRoom.password == password;
 
-    console.log(`roomId = ${roomId}
-         questionId = ${questionId}
-         action = ${action}
-         password = ${password}`);
+    // If password is valid, execute action
+    if (validPassword) {
+
+        if (action === 'delete') { // Delete action
+            await db.run(`DELETE FROM questions Where id = ${questionId}`);
+        } else { // Read action
+            await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+        }
+
+        await db.close();
+
+        res.redirect(`/room/${roomId}`);
+    }
 }
 
 module.exports = {
