33aea85c0e44fd76f66b545a2f794043d1a5cdde
Ismael Laet <ismaelabashi@hotmail.com>
2022-05-23 21:03:22 -0300

    Feat: Add create function (question)

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,3 +1,25 @@
+const Database = require("../database/config");
+
+const create = async (req, res) => {
+    const db = await Database();
+    const { roomId } = req.params;
+    const { question } = req.body;
+
+    await db.run(`INSERT INTO questions (
+        roomId,
+        title,
+        read
+    ) VALUES (
+        ${roomId},
+        "${question}",
+        0
+    )`);
+
+    await db.close();
+
+    res.redirect(`/room/${roomId}`);
+}
+
 const action = (req, res) => {
     const { roomId, questionId, action } = req.params;
     const { password } = req.body;
@@ -9,5 +31,6 @@ const action = (req, res) => {
 }
 
 module.exports = {
-    action
+    create,
+    action,
 }
--- src/route.js
+++ src/route.js
@@ -12,7 +12,9 @@ route.get('/room/:roomId', RoomController.open);
 
 route.post('/enter-room', RoomController.enter);
 
-route.post('/room/create-room', RoomController.create)
+route.post('/room/create-room', RoomController.create);
+
+route.post('/room/create/:roomId', QuestionController.create);
 
 route.post('/room/:roomId/:questionId/:action', QuestionController.action);
 
